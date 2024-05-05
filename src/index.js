5ef3cb942eebc790f3004478a648637f8cd07ab6
Sourav Das <souravdas10lm@gmail.com>
2024-05-05 18:17:16 +0530

    Add image prompt text in query parameters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import { Ai } from './vendor/@cloudflare/ai.js';
 
 export default {
-  async fetch(request, env) {
-    const ai = new Ai(env.AI);
+	async fetch(request, env) {
+		const ai = new Ai(env.AI);
 
-    const url = new URL(request.url);
+		const url = new URL(request.url);
 
-    const params = url.searchParams;
-    const promptText = params.get("prompt");
+		const params = url.searchParams;
+		const promptText = params.get('prompt');
 
-    const inputs = {
-      prompt: promptText ?? 'cyberpunk cat'
-    };
+		if (promptText && promptText.trim()) {
+			const inputs = {
+				prompt: promptText ?? 'cyberpunk cat',
+			};
 
-    const response = await ai.run(
-      '@cf/stabilityai/stable-diffusion-xl-base-1.0',
-      inputs
-    );
+			const response = await ai.run('@cf/stabilityai/stable-diffusion-xl-base-1.0', inputs);
 
-    return new Response(response, {
-      headers: {
-        'content-type': 'image/png'
-      }
-    });
-  }
+			return new Response(response, {
+				headers: {
+					'content-type': 'image/png',
+				},
+			});
+		} else {
+			return new Response("Please provide a text with 'prompt' query parameter to generate a image");
+		}
+	},
 };
